6a9994a4c78dc4a972fdad70e942d6d44fe129c7
Maciej Rutkowski <maciej.rutkowski11@gmail.com>
2020-03-05 00:33:02 +0100

    Working with first subpage, creating slideshow in js

--- slideShow.js
+++ slideShow.js
@@ -3,38 +3,47 @@ const dots = [...document.querySelectorAll(".dot")];
 
 const time = 5000;
 let active = 0;
-
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
 const changeSlide = () => {
     imgList[active].classList.remove('active')
     active++;
     if (active === imgList.length) {
         active = 0;
     }
-    imgList[active].classList.add('active')
+    imgList[active].classList.add('active');
+    changeDot()
 }
+
 let indexInterval = setInterval(changeSlide, time);
 
 const nextSlide = (e) => {
     imgList[active].classList.remove('active');
-    console.log("ok!")
+    // console.log(e.target.className)
     clearInterval(indexInterval);
-    e.target.value === next ? active++ : active--;
+    e.target.className === 'next' ? active++ : active--;
     if (active === imgList.length) {
         active = 0;
     } else if (active < 0) {
-        active = imgList - 1;
+        active = imgList.length - 1;
     }
     imgList[active].classList.add('active');
     indexInterval = setInterval(changeSlide, time);
+    changeDot()
 }
 
-// dots.forEach(dot => {
-//         dot.addEventListener('click', () => {
-//             imgList[active].classList.remove('active');
-//             clearInterval(indexInterval);
-//             console.log(dots[dot])
-//         })
-//     }
-// )
+dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+        imgList[active].classList.remove('active');
+        clearInterval(indexInterval);
+        active = dots.indexOf(dot);
+        imgList[active].classList.add('active');
+        indexInterval = setInterval(changeSlide, time);
+        changeDot()
+    })
+})
 const prev = document.querySelector('.prev').addEventListener('click', nextSlide);
 const next = document.querySelector('.next').addEventListener('click', nextSlide);
