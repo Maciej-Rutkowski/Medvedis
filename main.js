5e9016a2464a4fd28d4d3eb851c5955088720e14
Maciej Rutkowski <maciej.rutkowski11@gmail.com>
2020-03-08 21:28:45 +0100

    Adding new scrolling function

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ $('nav a, #home, #contact').on('click', function () {
         scrollTop: $(section).offset().top - $('.menu').height()
     })
 })
+
+$(document).ready(function () {
+    const queryString = window.location.search;
+    // console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const section = '[data-section=' + urlParams.get('id') + ']';
+    $("html,body").animate({
+        scrollTop: $(section).offset().top - $('.menu').height()
+    });
+});
