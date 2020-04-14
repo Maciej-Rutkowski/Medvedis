78c760842b8a4387b29ea6d764d6ff62d0e9aa3a
Maciej Rutkowski <maciej.rutkowski11@gmail.com>
2020-04-14 15:33:39 +0200

    Cleaning old comments

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-$('nav a, #home, #contact').on('click', function () {
-    const section = "[data-section=" + $(this).attr('id') + "]";
-    $('body, html').animate({
-        scrollTop: $(section).offset().top - $('.menu').height()
-    })
-})
+$("nav a, #home, #contact").on("click", function () {
+  const section = "[data-section=" + $(this).attr("id") + "]";
+  $("body, html").animate({
+    scrollTop: $(section).offset().top - $(".menu").height(),
+  });
+});
 
 $(document).ready(function () {
-    const queryString = window.location.search;
-    // console.log(queryString);
-    const urlParams = new URLSearchParams(queryString);
-    const section = '[data-section=' + urlParams.get('id') + ']';
-    $("html,body").animate({
-        scrollTop: $(section).offset().top - $('.menu').height()
-    });
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const section = "[data-section=" + urlParams.get("id") + "]";
+  $("html,body").animate({
+    scrollTop: $(section).offset().top - $(".menu").height(),
+  });
 });
--- slideShow.js
+++ slideShow.js
@@ -1,49 +1,52 @@
-const imgList = [...document.querySelectorAll('img.slider')];
+const imgList = [...document.querySelectorAll("img.slider")];
 const dots = [...document.querySelectorAll(".dot")];
 
 const time = 5000;
 let active = 0;
 const changeDot = () => {
-    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
-    dots[activeDot].classList.remove('active');
-    dots[active].classList.add('active');
-}
+  const activeDot = dots.findIndex((dot) => dot.classList.contains("active"));
+  dots[activeDot].classList.remove("active");
+  dots[active].classList.add("active");
+};
 const changeSlide = () => {
-    imgList[active].classList.remove('active')
-    active++;
-    if (active === imgList.length) {
-        active = 0;
-    }
-    imgList[active].classList.add('active');
-    changeDot()
-}
+  imgList[active].classList.remove("active");
+  active++;
+  if (active === imgList.length) {
+    active = 0;
+  }
+  imgList[active].classList.add("active");
+  changeDot();
+};
 
 let indexInterval = setInterval(changeSlide, time);
 
 const nextSlide = (e) => {
-    imgList[active].classList.remove('active');
-    // console.log(e.target.className)
-    clearInterval(indexInterval);
-    e.target.className === 'next' ? active++ : active--;
-    if (active === imgList.length) {
-        active = 0;
-    } else if (active < 0) {
-        active = imgList.length - 1;
-    }
-    imgList[active].classList.add('active');
-    indexInterval = setInterval(changeSlide, time);
-    changeDot()
-}
+  imgList[active].classList.remove("active");
+  clearInterval(indexInterval);
+  e.target.className === "next" ? active++ : active--;
+  if (active === imgList.length) {
+    active = 0;
+  } else if (active < 0) {
+    active = imgList.length - 1;
+  }
+  imgList[active].classList.add("active");
+  indexInterval = setInterval(changeSlide, time);
+  changeDot();
+};
 
-dots.forEach(dot => {
-    dot.addEventListener('click', () => {
-        imgList[active].classList.remove('active');
-        clearInterval(indexInterval);
-        active = dots.indexOf(dot);
-        imgList[active].classList.add('active');
-        indexInterval = setInterval(changeSlide, time);
-        changeDot()
-    })
-})
-const prev = document.querySelector('.prev').addEventListener('click', nextSlide);
-const next = document.querySelector('.next').addEventListener('click', nextSlide);
+dots.forEach((dot) => {
+  dot.addEventListener("click", () => {
+    imgList[active].classList.remove("active");
+    clearInterval(indexInterval);
+    active = dots.indexOf(dot);
+    imgList[active].classList.add("active");
+    indexInterval = setInterval(changeSlide, time);
+    changeDot();
+  });
+});
+const prev = document
+  .querySelector(".prev")
+  .addEventListener("click", nextSlide);
+const next = document
+  .querySelector(".next")
+  .addEventListener("click", nextSlide);
